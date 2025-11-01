9481fe6c9cc9c8155ef4caa057874a5d92f31429
MeDear0-0 <pimpajee.seth@mail.kmutt.ac.th>
2025-11-02 00:43:27 +0700

    fix error from feV1

--- src/main/java/com/nw2/parcel/configs/SecurityConfig.java
+++ src/main/java/com/nw2/parcel/configs/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(a -> a
-                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/public/").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new FirebaseAuthFilter(), UsernamePasswordAuthenticationFilter.class);
