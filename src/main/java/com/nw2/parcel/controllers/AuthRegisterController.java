fd0ad0dd4b48d8c6b3c4579ce59b26ec6fc7f247
MeDear0-0 <pimpajee.seth@mail.kmutt.ac.th>
2025-11-02 02:09:42 +0700

    configs fix error from fe

--- src/main/java/com/nw2/parcel/controllers/AuthRegisterController.java
+++ src/main/java/com/nw2/parcel/controllers/AuthRegisterController.java
@@ -9,7 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = {"http://localhost:5173","http://192.168.103.151:5173","http://localhost:5174","http://192.168.103.151:5174"})
+@CrossOrigin(origins = {"*","http://localhost:5173","http://192.168.103.151:5173","http://localhost:5174","http://192.168.103.151:5174"})
 
 @RestController
 @RequestMapping("/public/auth")  // endpoint สำหรับสมัคร (ไม่ต้อง auth)
