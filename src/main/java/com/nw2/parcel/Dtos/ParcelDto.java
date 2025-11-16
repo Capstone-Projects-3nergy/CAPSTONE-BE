5ed93752485af0efaa0efb7ad7cfb28b2b77b0eb
MeDear0-0 <pimpajee.seth@mail.kmutt.ac.th>
2025-11-17 02:17:21 +0700

    set endpoint company for add parcelV2.1

---
+++ src/main/java/com/nw2/parcel/Dtos/ParcelDto.java
@@ -0,0 +1,28 @@
+package com.nw2.parcel.Dtos;
+
+import com.nw2.parcel.entity.Parcels;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParcelDto {
+    private Integer parcelId;
+    private String trackingNumber;
+    private String recipientName;
+    private Parcels.Status status;
+    private String parcelType;
+    private String senderName;
+
+    private Integer companyId;
+    private String companyName;
+
+    private Integer userId;   // owner (resident)
+
+    // ถ้าอยากจะใส่เวลาเพิ่มก็เติมได้ เช่น
+    // private LocalDateTime receivedAt;
+    // private LocalDateTime pickedUpAt;
+    // private LocalDateTime updatedAt;
+}
--- src/main/java/com/nw2/parcel/controllers/ParcelController.java
+++ src/main/java/com/nw2/parcel/controllers/ParcelController.java
@@ -1,6 +1,7 @@
 package com.nw2.parcel.controllers;
 
 import com.nw2.parcel.Dtos.CreateParcelDto;
+import com.nw2.parcel.Dtos.ParcelDto;
 import com.nw2.parcel.entity.Parcels;
 import com.nw2.parcel.services.ParcelService;
 import lombok.RequiredArgsConstructor;
@@ -22,8 +23,20 @@ public class ParcelController {
     //add
     @PostMapping("/add")
     @ResponseStatus(HttpStatus.CREATED)
-    public Parcels createParcel(@RequestBody CreateParcelDto req) {
-        return parcelService.createParcel(req);
+    public ParcelDto createParcel(@RequestBody CreateParcelDto req) {
+        Parcels p = parcelService.createParcel(req);
+
+        return new ParcelDto(
+                p.getParcelId(),
+                p.getTrackingNumber(),
+                p.getRecipientName(),
+                p.getStatus(),
+                p.getParcelType(),
+                p.getSenderName(),
+                p.getCompany() != null ? p.getCompany().getCompanyId() : null,
+                p.getCompany() != null ? p.getCompany().getCompanyName() : null,
+                p.getUser() != null ? p.getUser().getUserId() : null
+        );
     }
 
     // ต่อไปจะมี GET /api/parcels, GET /api/parcels/{id}, PUT, PATCH /status ฯลฯ ได้อีก
