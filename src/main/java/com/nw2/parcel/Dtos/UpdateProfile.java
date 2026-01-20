5b0a4b964564c4b011fc94672075b3b0305022d5
MeDear0-0 <pimpajee.seth@mail.kmutt.ac.th>
2026-01-21 01:05:20 +0700

    edit profile user3.2

--- src/main/java/com/nw2/parcel/Dtos/UpdateProfile.java
+++ src/main/java/com/nw2/parcel/Dtos/UpdateProfile.java
@@ -4,15 +4,10 @@ import lombok.Data;
 
 @Data
 public class UpdateProfile {
-
     private String firstName;
     private String lastName;
     private String phoneNumber;
     private String lineId;
-
-    // resident only
     private String roomNumber;
-
-    // staff only
     private String position;
 }
